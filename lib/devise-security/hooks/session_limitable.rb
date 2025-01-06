958d41554c052502f163c0a071df03a858c5590a
Tham <tham@tomosia.com>
2025-01-06 07:17:10 +0700

    updates

--- lib/devise-security/hooks/session_limitable.rb
+++ lib/devise-security/hooks/session_limitable.rb
@@ -37,7 +37,7 @@ Warden::Manager.after_set_user only: :fetch do |record, warden, options|
         "actual=#{warden.session(scope)['unique_session_id'].inspect}"
       end
       # warden.raw_session.clear
-      # warden.logout(scope)
+      warden.logout(scope)
       throw :warden, scope: scope, message: :session_limited
     end
   end
